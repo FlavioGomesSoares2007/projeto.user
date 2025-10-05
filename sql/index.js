25473ebea1b672edf1e14786617bdfc02b2c4cab
FLAVIO GOMES SOARES <FlavioGomesSoares2007@gmail.com>
2025-10-05 14:54:32 -0300

    se Deus quiser

--- sql/index.js
+++ sql/index.js
@@ -1,18 +1,10 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-  logging: false
+const conn = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false
 });
 
-try {
-  await sequelize.authenticate();
-  console.log('banco conectado');
-} catch (error) {
-  console.error('Erro ao conectar no banco:', error);
-}
-
-export default sequelize;
+export default conn;
